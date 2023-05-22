f41d86ace788141f09480543713312aff346503e
weibin.xu <xiaobin1124@gmail.com>
2023-05-22 11:27:09 +0800

    添加桌面页面，类似操作系统

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import './css/x.css'
 import Calendar from './page/calendar/Calendar';
@@ -6,35 +7,60 @@ import Scene from './page/flow/Scene';
 import HttpReqCompare from './page/hankyu/HttpReqCompare';
 import Todo from './page/todo/Todo';
 import Todo2 from './page/todo/Todo2';
+import globalId from './util/globalId';
 
+const apps = [
+  {
+    _id: 'todo', 
+    name: 'To-Do',
+    render: () => {
+      return (
+        <div key={globalId()}>
+          <Todo />
+        </div>
+      )
+    }
+  }
+]
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-      //<Scene />
+  const [renderText, setRenderText] = useState('')
+  const [appOpen, setAppOpen] = useState(false);
+
+  function openApp(item){
+    setRenderText(item.render())
+    setAppOpen(true);
+  }
+
   return (
-    <div>
-      <Todo />
-      {/* <Calendar props={{calendar: {}}}/> */}
-      {/* <Order /> */}
-      {/* <HttpReqCompare /> */}
+    <div className="App">
+      {
+        !appOpen && 
+          <div style={{ padding: '10px' }}>
+            {
+              apps.map(a => {
+                return (
+                  <div key={a._id} className='app-icon pointer' onDoubleClick={event => openApp(a)}>
+                    <span>{a.name}</span>
+                  </div>
+                )
+              })
+            }
+          </div>
+      }
+      {
+        !!appOpen && [renderText]
+      }
     </div>
-  )
+  );
+      //<Scene />
+  // return (
+  //   <div>
+  //     <Todo />
+  //     {/* <Calendar props={{calendar: {}}}/> */}
+  //     {/* <Order /> */}
+  //     {/* <HttpReqCompare /> */}
+  //   </div>
+  // )
 }
 
 export default App;
