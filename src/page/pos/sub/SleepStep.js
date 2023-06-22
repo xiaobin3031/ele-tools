5e82da786fa2c998e4538c613647dfea45cd31ba
xiaobin <xiaobin1124@gmail.com>
2023-06-22 16:25:00 +0000

    sleepStep.js

--- src/page/pos/sub/SleepStep.js
+++ src/page/pos/sub/SleepStep.js
@@ -1,9 +1,18 @@
+import Input from "../../../component/Input";
+import Label from "../../../component/Label";
+import Row from "../../../component/Row";
 
-export default function SleepStep({}){
+export default function SleepStep({_step, _stepChange}){
+
+  function valueChange(event){
+    _step[event.target.name] = event.target.value;
+    _stepChange({..._step})
+  }
 
   return (
-    <div>
-
-    </div>
+    <Row>
+      <Label size="sm">睡眠时间/秒</Label>
+      <Input style={{width: '100px'}} size="sm" value={_step.sleep} name="sleep" onChange={valueChange} />
+    </Row>
   )
 }
