3da11d723409459adcc9080fa0d32557e9709b9d
xiaobin <xiaobin1124@gmail.com>
2023-06-29 14:39:31 +0000

    增加pos服务端的回调功能，以及自定义的数据类型

--- remote/pos/posAutoTest.js
+++ remote/pos/posAutoTest.js
@@ -7,12 +7,29 @@ const logHandle = require('./handleLog.js')
 const flowHandle = require('./handleFlow.js')
 
 // 下标类型 begin
-const INDEX_CONTENT_LEN = 30; // 报文内容长度的起始
+// const INDEX_DATATYPE = 0; // 报文类型的下标
+// const INDEX_CLIENT_ID = INDEX_DATATYPE + LEN_DATA_TYPE;  // 客户端id的下标
+// const INDEX_RESERVE = INDEX_CLIENT_ID + LEN_CLIENT_ID;  // 预留字段的下标
+// const INDEX_CONTENT_LEN = INDEX_RESERVE + LEN_RESERVE; // 报文内容长度的起始
+// const INDEX_CONTENT = INDEX_CONTENT_LEN + LEN_CONTENT_LEN; // 报文内容的下标
 // 下标类型 end
+
+// 报文长度 begin
+const LEN_DATA_TYPE = 1;   // 报文类型长度
+const LEN_CLIENT_ID = 32;   // 客户端id长度
+const LEN_RESERVE = 32;  // 预留字段长度
+const LEN_CONTENT_LEN = 4;  // 报文长度的长度
+// 报文长度 end
 
 // 内容类型 begin
 const DATATYPE_CONNECT = 1;  // 第一次连接注册设备id
+const DATATYPE_REQ_FLOWS = 2;  //请求流程信息
+const DATATYPE_LOG = 3; // 日志信息
+const DATATYPE_LOG_STEP = 4; // 单步日志信息
 // 内容类型 end
+
+const connectCallbacks = [];
+const responseCallbacks = {};
 
 /**
  * 获取文本的长度
@@ -80,10 +97,10 @@ const sendError = (socket, msg) => {
 }
 
 let server;
-const clients = []; // 客户端集合
+const clients = {};
 function startServer(){
+  connectCallbacks = []
   server = net.createServer((socket) => {
-    clients.push(socket);
     const ipLogMap = {};   //一个客户端只能有一个globalLogId存在
     console.log('connect: ', socket.remoteAddress, ':', socket.remotePort);
 
@@ -92,31 +109,45 @@ function startServer(){
       // 调整请求报文的格式
       let offset = 0; // 有可能多个报文同时接受到，此时会有偏移值
       while(true) {
-        const dataType = data.readInt8(0 + offset);
+        const dataType = data.readInt8(offset);
         if(dataType >= DATATYPE_CONNECT){
-          const clientId = data.toString("utf-8", 1 + offset, 17 + offset);
-          const len = data.readInt32BE(INDEX_CONTENT_LEN + offset);
+          offset += LEN_DATA_TYPE
+          const clientId = data.toString("utf-8", offset, offset + LEN_CLIENT_ID);
+          if(dataType === DATATYPE_CONNECT){
+            clients[clientId] = socket;
+            if(connectCallbacks.length > 0){
+              connectCallbacks.forEach(a => a({clientId: clientId}))
+            }
+          }
+          offset += LEN_CLIENT_ID
+          offset += LEN_RESERVE
+          const len = data.readInt32BE(offset);
           if(len > 0){
-            const msg = data.toString("utf-8", 35 + offset, len + 35 + offset);
+            offset += LEN_CONTENT_LEN
+            const msg = data.toString("utf-8", offset, offset + len);
             switch(dataType){
-              case 1:
+              case DATATYPE_REQ_FLOWS:
                 clientHandle(msg, socket.remoteAddress)
                   .then(content => {
                     sendSuccess(socket, JSON.stringify(content));
                   }).catch(err => sendError(socket, JSON.stringify(err)));
                 break;
-              case 2:
+              case DATATYPE_LOG:  //日志
                 const globalLogId = logHandle(msg);
                 if(!!globalLogId){
                   ipLogMap[socket.remoteAddress] = globalLogId;
                 }
                 break;
-              case 3:
+              default:
+                const cbs = responseCallbacks[dataType];
+                if(!!cbs && cbs.length > 0){
+                  cbs.forEach(cb => cb(msg)) // 进行回调处理
+                }
                 break;
             }
           }
-          offset += len + 35;
-          if(data.length <= offset){
+          offset += len;
+          if(data.length - 1 <= offset){
             break;
           }
         }else{
@@ -138,10 +169,10 @@ function startServer(){
     })
 
     const logEnd = (endReason) => {
-      for(let i=0;i<clients.length;i++){
-        if(clients[i] === socket){
-          clients.splice(i, 1)
-          break
+      for(let k in clients){
+        if(clients[k] === socket){
+          delete clients[k]
+          break;
         }
       }
       // 客户端断开后，标识记录日志为空
@@ -165,19 +196,46 @@ function startServer(){
     console.log('server closed success')
   })
 }
-
+/**
+ * 关闭服务
+ * @param {function} cb onceConnect()注入的回调函数
+ */
 function stopServer(){
-  if(clients.length > 0){
-    clients.forEach(socket => socket.close())
+  for(let k in clients){
+    clients[k].close();
+    delete clients[k]
   }
   if(server){
     console.log('prepare to close server')
     server.close();
   }
+  connectCallbacks = []
+  responseCallbacks = []
+}
+
+function sendContent({clientId, content}){
+  if(!content || !clientId) return
+  const socket = clients[clientId]
+  if(!socket) return
+  sendSuccess(socket, content)
 }
 
 module.exports = {
   start: startServer,
   stop: stopServer,
-  onceConnect: 
+  onceConnect: function(cb){
+    if(!!cb && typeof cb === 'function' && connectCallbacks.every(a => a !== cb)){
+      connectCallbacks.push(cb);
+    }
+  },
+  registerResponse: function(dataType, cb){
+    if(dataType in responseCallbacks){
+      if(responseCallbacks[dataType].every(a => a !== cb)){
+        responseCallbacks[dataType].push(cb);
+      }
+    }else{
+      responseCallbacks[dataType] = [cb];
+    }
+  },
+  sendContent: sendContent
 }
